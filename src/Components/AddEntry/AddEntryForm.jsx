9a0009fafb162909629e6468e1da0bcf6cee84ee
Dyshine Hill <eyedolarts@gmail.com>
2022-04-19 21:22:24 -0500

    completed add entry component

---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+
+
+
+
+
+
+
+const  AddEntryForm= (props) => {
+
+        const [weight, setWeight] = useState(0);
+        const[date, setDate] = useState('')
+
+        function handlesSubmit(event){
+            event.preventDefault();
+            let newEntry ={
+                weight:weight,
+                date:date
+            };
+            console.log(newEntry);
+            props.addNewEntryProperty(newEntry)
+        }
+
+    return (   
+     
+        <form onSubmit={handlesSubmit}>
+            <label>Weight</label>
+            <input type='number' value={weight} onChange={(event) => setWeight(parseFloat(event.target.value))}/>
+            <label>Date</label>
+            <input type= 'date' value={date} onChange={(event) => setDate(event.target.value)}/>
+            <button type='submit'>add</button>
+        </form>
+    );
+}
+ 
+export default AddEntryForm;
+
+
+
+
+
+
+
